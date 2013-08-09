c69019261c02230b9c23158e0d81431578ade4dc
Matthew McFarland <mmcfarland@azavea.com>
2013-08-09 19:09:56 -0400

    Tighten up the display

--- map.js
+++ map.js
@@ -4,7 +4,7 @@
         self = this;
         self.directionsService = new google.maps.DirectionsService();
         self.directionsDisplay = new google.maps.DirectionsRenderer();
-
+        self.routeUrl = "http://www.mapquestapi.com/directions/v1/route?key=Fmjtd%7Cluub2561nq%2Caa%3Do5-9u8xuz&from={{from}}&to=39.875,-75.238";
         var mapOptions = {
              center: new google.maps.LatLng(39.875, -75.238),
              zoom: 13,
@@ -30,8 +30,12 @@
 
         self.directionsService.route(request, function(result, status) {
             if (status == google.maps.DirectionsStatus.OK) {
-              self.directionsDisplay.setDirections(result);
+                self.directionsDisplay.setDirections(result);
+                self.trigger('route-time-update', result.routes[0].legs[0]);
             }
         });
     };
+
+    _.extend(N.Map.prototype, Backbone.Events);
+
 }(this));
--- ready.js
+++ ready.js
@@ -2,10 +2,25 @@
     N.Ready = function(opts) {
         var self = this;
 
-        self.flight = opts.flight;
-        self.direction = opts.direction; 
         self.server = 'http://54.235.132.110/';
         self.map = new Map();
+
+        var Flight = Backbone.Model.extend({}),
+            FlightV = Backbone.View.extend({
+                initialize: function() {
+                    this.model.on('change', this.render, this);
+                },
+
+                render: function() {
+                    var t = _.template($('#template-flight').html());
+                        ctx = this.model.toJSON();
+                        landsAt = moment(ctx.scheduledDateTime),
+                        landsIn = moment();
+                    this.$el.html(t(ctx));
+                    this.$el.appendTo('body');
+                    return this;
+                }
+        });
 
         var socket = io.connect(self.server);
 
@@ -13,15 +28,34 @@
             console.log(data);
         });
 
-        $('#go').click($.proxy(self.map.calculateRoute, self.map));
+        self.map.on('route-time-update', function(route) {
+            $('#addr').val(route.start_address);
+            self.flight.set('travelTime', route.duration.value);
+            console.log(route);
+        });
+
+        $('#go').click(function() {
+            self.fn = $('#flight-num').val();
+            self.direction = $('#direction').val() || "Arrival";
+
+            $.getJSON(self.server + 'number/' + self.fn + '?callback=?', function(status) {
+                if (status.length > 0) {
+                    self.flight = new Flight(status[0]);
+                    self.flightv = new FlightV({ model: self.flight }).render();
+
+                    self.map.calculateRoute();
+
+                } else {
+                    alert("Sorry, couldn't find that flight.");
+                }
+            });
+        });
+
         socket.emit('subscribe', {
-            "flightNumber": self.flight, 
+            "flightNumber": self.fn, 
             "direction": self.direction
         });
 
-        $.getJSON(self.server + 'number/' + self.flight + '?callback=?', function(status) {
-            console.log(status);
-        });
 
     
     };
