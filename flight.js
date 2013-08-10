dc43424644d2904882c1b55d7ef9bf86fa84a2a3
Matthew McFarland <mmcfarland@gmail.com>
2013-08-10 12:04:53 -0400

    Better formatting for time

--- flight.js
+++ flight.js
@@ -5,18 +5,33 @@
             this.on('change', this.computeTime, this);
         }, 
 
+        formatMins: function(mins) {
+            if (mins < 60) return mins + " mins";
+            var h = Math.floor(mins/60),
+                m = Math.ceil(mins % 60),
+                fh =  h + ' hr' + (h > 1 ? 's':''),
+                fm = m > 0 ? ' and ' + m + ' mins' : '';
+
+            return fh + fm;
+        },
+
         computeTime: function() {
             var landsAt = moment(this.get('scheduledDateTime')),
                 landsIn = landsAt.diff(moment(), 'minutes'),
                 leaveIn = landsAt.diff(moment()
                     .add(this.get('travelTime'), 'minutes'), 'minutes'),
-                leaveAt = moment().add(leaveIn, 'minutes').format('h:mm:ss a');
+                leaveInF = this.formatMins(leaveIn),
+                leaveAt = moment().add(leaveIn, 'minutes').format('h:mm a'),
+                travelTime = this.get('travelTime') || 0,
+                travelTimeF = this.formatMins(travelTime); 
 
             this.set({
                 landsAt: landsAt,
                 landsIn: landsIn,
                 leaveIn: leaveIn,
-                leaveAt: leaveAt
+                leaveInF: leaveInF,
+                leaveAt: leaveAt,
+                travelTimeF: travelTimeF
             });
 
             this.trigger('updated');
@@ -34,7 +49,7 @@
                 ctx = this.model.toJSON();
             console.log(ctx);
             this.$el.empty().html(t(ctx));
-            this.$el.appendTo(this.$c);
+            this.$el.appendTo(this.$c.empty());
             return this;
         }
     });
