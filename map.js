4031353f31533eb9bfe5a91fb75866d45240d56f
Matthew McFarland <mmcfarland@gmail.com>
2013-08-10 12:05:33 -0400

    Directions and refresh
    Mapquest was in there because i misunderstood what google gave
    for free api customers re: travel time

--- map.js
+++ map.js
@@ -4,17 +4,21 @@
         self = this;
         self.directionsService = new google.maps.DirectionsService();
         self.directionsDisplay = new google.maps.DirectionsRenderer();
-        self.routeUrl = "http://www.mapquestapi.com/directions/v1/route?key=Fmjtd%7Cluub2561nq%2Caa%3Do5-9u8xuz&from={{from}}&to=39.875,-75.238";
+
         var mapOptions = {
              center: new google.maps.LatLng(39.875, -75.238),
              zoom: 13,
              mapTypeId: google.maps.MapTypeId.ROADMAP
         };
 
-        var map = new google.maps.Map(document.getElementById("map"),
-            mapOptions);
+        var map = new google.maps.Map(document.getElementById("map"),mapOptions),
+            traffic = new google.maps.TrafficLayer();
 
+        traffic.setMap(map);
         self.directionsDisplay.setMap(map);
+
+        // Update the travel times ever minute
+        setInterval(self.calculateRoute, 60 * 1000);
     };
 
     N.Map.prototype.calculateRoute = function() {
